a0c80852acb8eb4b1fd77d922d17acfe2502daf8
Lendar <xeldim@gmail.com>
2014-07-30 21:56:33 +0400

    add `grunt serve` with watch and live error reporting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-webpack');
     grunt.initConfig({
         webpack: {
@@ -10,8 +11,28 @@ module.exports = function (grunt) {
                     path: 'dist/'
                 }
             }
+        },
+        open: {
+            options: {
+                delay: 500
+            },
+            dev: {
+                path: 'http://localhost:9000/webpack-dev-server/demo.html'
+            }
+        },
+        'webpack-dev-server': {
+            options: {
+                port: 9000,
+                webpack: '<%= webpack.dist %>',
+                publicPath: '/dist/',
+                contentBase: './',
+            },
+            dev: {
+                keepAlive: true,
+            }
         }
     });
 
     grunt.registerTask('default', ['webpack:dist']);
+    grunt.registerTask('serve', ['open:dev', 'webpack-dev-server']);
 };
